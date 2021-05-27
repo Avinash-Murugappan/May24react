ddad7596309fbcdd121ae774e56a5f401d6f4c57
Avinash A Murugappan <58096840+Avinash-Murugappan@users.noreply.github.com>
2021-05-27 13:18:37 -0400

    added homework 3 links

--- src/Components/Homeworks/index.js
+++ src/Components/Homeworks/index.js
@@ -20,7 +20,8 @@ function Homeworks() {
           <th>#</th>
           <th>Type</th>
           <th>Description</th>
-          <th>Link</th>
+          <th>Result Link</th>
+          <th>Github Link</th>
         </tr>
       </thead>
       <tbody>
@@ -29,6 +30,7 @@ function Homeworks() {
           <td>Homework1</td>
           <td>Git Practice. Provide link of your repo here </td>
           <td><a href="https://github.com/Avinash-Murugappan/git-practice/tree/main"> Link</a> </td>
+          <td> None</td>
 
       </tr>
         <tr>
@@ -36,6 +38,7 @@ function Homeworks() {
           <td>Homework 2</td>
           <td>Javascript - Here is my codepen link with solutions</td>
           <td><a href="https://codepen.io/avinash-murugappan/pen/xxqdeRB"> Link</a> </td>
+          <td>None</td>
 
       </tr>
       <tr>
@@ -43,6 +46,7 @@ function Homeworks() {
           <td>Homework 2.1</td>
           <td>Local and Session Storage </td>
           <td><a href="https://codepen.io/avinash-murugappan/pen/zYZdqev"> Link</a> </td>
+          <td>None</td>
       </tr>
 
       <tr>
@@ -50,7 +54,39 @@ function Homeworks() {
           <td>Homework 2.2</td>
           <td>Build a Wynisco Bootcamp Class Website</td>
           <td><a href=" https://avinash-homrwork-2-2.netlify.app/"> Link</a> </td>
-      </tr>
+          <td> <a href="https://github.com/Avinash-Murugappan/Homework2-2">Link</a></td>
+                </tr>
+
+            <tr>
+          <th scope="row">3</th>
+          <td>Homework 3.1</td>
+          <td>CSS Selectors Exercise</td>
+          <td><a href="https://codepen.io/avinash-murugappan/pen/JjWywGZ"> Link</a> </td>
+          <td> None </td>
+                </tr>
+<tr>
+                 <th scope="row">3</th>
+          <td>Homework 3</td>
+          <td>3HW Quiz</td>
+          <td><a href="https://codepen.io/avinash-murugappan/pen/ZEeJwoZ"> Link</a> </td>
+          <td> None </td>
+                </tr>
+
+                 <tr>
+          <th scope="row">3</th>
+          <td>Homework 3.2</td>
+          <td>Navigation Bar</td>
+          <td><a href="https://homework3-2.netlify.app/"> Link</a> </td>
+          <td><a href="https://github.com/Avinash-Murugappan/Homework3.2-NavigationBar/"> Link</a> </td>
+                </tr>
+
+                  <tr>
+          <th scope="row">3</th>
+          <td>Homework 3.3</td>
+          <td>Javascript Function</td>
+          <td><a href="https://codepen.io/avinash-murugappan/pen/YzZxgKx"> Link</a> </td>
+          <td> None </td>
+                </tr>
       </tbody>
     </Table>
         </Container>
